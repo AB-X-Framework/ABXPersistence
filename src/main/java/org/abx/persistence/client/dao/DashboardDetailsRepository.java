1f6f75e3fbb83f5ac9a18dab1fa29ce66e859e20
luis <llara@appvance.com>
2025-02-21 10:50:51 -0600

    ABX Persistence

---
+++ src/main/java/org/abx/persistence/client/dao/DashboardDetailsRepository.java
@@ -0,0 +1,15 @@
+package org.abx.persistence.client.dao;
+
+import org.abx.persistence.client.model.DashboardDetails;
+import org.abx.persistence.client.model.Enrollment;
+import org.abx.persistence.client.model.UserDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DashboardDetailsRepository  extends JpaRepository<DashboardDetails, Long> {
+    @Override
+    void delete(DashboardDetails dashboardDetails);
+
+
+    DashboardDetails findByDashboardId(long dashboardId);
+
+}
