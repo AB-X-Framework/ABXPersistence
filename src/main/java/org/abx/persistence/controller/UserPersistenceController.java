a5d8322847c47a4e186ab6f7a6f76406154bab7e
Luis Carlos <l3clara@gmail.com>
2025-03-17 10:39:12 -0600

    ABX Persistence

--- src/main/java/org/abx/persistence/controller/UserPersistenceController.java
+++ src/main/java/org/abx/persistence/controller/UserPersistenceController.java
@@ -3,10 +3,12 @@ package org.abx.persistence.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.abx.persistence.client.DashboardPersistenceManager;
 import org.abx.persistence.client.ProjectPersistenceManager;
-import org.abx.persistence.client.RepoPersistenceManager;
 import org.abx.persistence.client.UserPersistenceManager;
 import org.abx.persistence.client.model.UserDetails;
+import org.abx.spring.ErrorMessage;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserPersistenceController {
     @Autowired
     private UserPersistenceManager userPersistenceManager;
+
+
+    @Autowired
+    private ProjectPersistenceManager projectPersistenceManager;
 
     @Autowired
     private DashboardPersistenceManager dashboardPersistenceManager;
@@ -27,4 +33,17 @@ public class UserPersistenceController {
         return details.getUsername();
     }
 
+    @RequestMapping(value = "/menu", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured({"UseABX"})
+    public String menu(final HttpServletRequest request) throws Exception{
+        try {
+            String user = request.getUserPrincipal().getName();
+            JSONObject jsonMenu = new JSONObject();
+            jsonMenu.put("dashboards",dashboardPersistenceManager.getDashboards(user));
+            jsonMenu.put("projects",projectPersistenceManager.projectEnrollments(user));
+            return jsonMenu.toString();
+        }catch (Exception e){
+            return ErrorMessage.errorString("Cannot get user information");
+        }
+    }
 }
